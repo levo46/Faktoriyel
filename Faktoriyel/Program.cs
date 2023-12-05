79a9f5ae3d5a02a2cf0c9ff6e6564dab8442e2c3
levent581 <levent581@BPC03>
2023-12-05 11:10:02 +0300

    Proje dosyası ekle.

---
+++ Faktoriyel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel
+{
+    class Faktoriyelbul
+    {
+        public int FakBul(int sayi)
+        {
+            int hesap = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                hesap = hesap * i;
+            }
+            return hesap;
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Faktoriyelbul s = new Faktoriyelbul();
+            Console.WriteLine("Faktoriyel bul = ");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Sonuç=" + s.FakBul(sayi));
+        }
+    }
+}
